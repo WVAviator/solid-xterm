4a4030ccbaa3a7041075359960ab352a7d75450f
Wvaviator <wvaviator@gmail.com>
2023-11-18 20:13:25 -0600

    expose more functionality on xterm library

--- src/XTerm.tsx
+++ src/XTerm.tsx
@@ -1,31 +1,67 @@
 import { createEffect, onCleanup } from 'solid-js';
-import { Terminal } from 'xterm';
+import {
+  ITerminalAddon,
+  ITerminalInitOnlyOptions,
+  ITerminalOptions,
+  Terminal,
+} from 'xterm';
 import '../node_modules/xterm/css/xterm.css';
+import useTerminal from './useTerminal';
 
 interface XTermProps {
+  class?: string;
+  options?: ITerminalOptions & ITerminalInitOnlyOptions;
+  addons?: ITerminalAddon[];
   onBell?: () => void;
   onBinary?: (data: string) => void;
   onCursorMove?: (cursorPosition: { x: number; y: number }) => void;
   onData?: (data: string) => void;
+  onKey?: (event: { key: string; domEvent: KeyboardEvent }) => void;
+  onLineFeed?: () => void;
+  onRender?: (event: { start: number; end: number }) => void;
+  onResize?: (size: { cols: number; rows: number }) => void;
+  onScroll?: (yPos: number) => void;
+  onSelectionChange?: () => void;
+  onTitleChange?: (title: string) => void;
+  onWriteParsed?: () => void;
 }
 
 const XTerm = ({
+  class: className = '',
+  options = {},
+  addons = [],
   onBell = () => {},
   onBinary = () => {},
   onCursorMove = () => {},
   onData = () => {},
+  onKey = () => {},
+  onLineFeed = () => {},
+  onRender = () => {},
+  onResize = () => {},
+  onScroll = () => {},
+  onSelectionChange = () => {},
+  onTitleChange = () => {},
+  onWriteParsed = () => {},
 }: XTermProps) => {
   let terminalContainerRef: HTMLDivElement | undefined;
   let terminal: Terminal | undefined;
 
+  const { setTerminal } = useTerminal();
+
   createEffect(() => {
-    if (!terminalContainerRef) {
-      return;
-    }
-    terminal = new Terminal();
+    if (!terminalContainerRef) return;
+
+    terminal = new Terminal(options);
     terminal.open(terminalContainerRef);
 
+    addons.forEach((addon) => {
+      terminal?.loadAddon(addon);
+    });
+
+    setTerminal(terminal);
+
     onCleanup(() => {
+      setTerminal(undefined);
       terminal?.dispose();
     });
   });
@@ -68,7 +104,75 @@ const XTerm = ({
     });
   });
 
-  return <div ref={terminalContainerRef}></div>;
+  createEffect(() => {
+    if (!terminal) return;
+    const onKeyListener = terminal.onKey((event) => onKey(event));
+    onCleanup(() => {
+      onKeyListener.dispose();
+    });
+  });
+
+  createEffect(() => {
+    if (!terminal) return;
+    const onLineFeedListener = terminal.onLineFeed(() => onLineFeed());
+    onCleanup(() => {
+      onLineFeedListener.dispose();
+    });
+  });
+
+  createEffect(() => {
+    if (!terminal) return;
+    const onRenderListener = terminal.onRender((event) => onRender(event));
+    onCleanup(() => {
+      onRenderListener.dispose();
+    });
+  });
+
+  createEffect(() => {
+    if (!terminal) return;
+    const onResizeListener = terminal.onResize((size) => onResize(size));
+    onCleanup(() => {
+      onResizeListener.dispose();
+    });
+  });
+
+  createEffect(() => {
+    if (!terminal) return;
+    const onScrollListener = terminal.onScroll((yPos) => onScroll(yPos));
+    onCleanup(() => {
+      onScrollListener.dispose();
+    });
+  });
+
+  createEffect(() => {
+    if (!terminal) return;
+    const onSelectionChangeListener = terminal.onSelectionChange(() =>
+      onSelectionChange()
+    );
+    onCleanup(() => {
+      onSelectionChangeListener.dispose();
+    });
+  });
+
+  createEffect(() => {
+    if (!terminal) return;
+    const onTitleChangeListener = terminal.onTitleChange((title) =>
+      onTitleChange(title)
+    );
+    onCleanup(() => {
+      onTitleChangeListener.dispose();
+    });
+  });
+
+  createEffect(() => {
+    if (!terminal) return;
+    const onWriteParsedListener = terminal.onWriteParsed(() => onWriteParsed());
+    onCleanup(() => {
+      onWriteParsedListener.dispose();
+    });
+  });
+
+  return <div class={className} ref={terminalContainerRef}></div>;
 };
 
 export default XTerm;
